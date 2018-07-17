5a12d6fd3a011bd53dedac6c9da535948bf7bbeb
Erwan Riou <rr.1.riou@gmail.com>
2018-07-17 18:34:57 +0200

    build: building route of our future API

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 
+//port
 const PORT = 3001
+
+//routes
+app.get('/movies', (req, res) => {
+  res.send('Bientot des films ici meme')
+})
+app.get('/movies/add', (req, res) => {
+  res.send('Ajouter des films ici même')
+})
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`film numéro ${id}`)
+})
 app.get('/', (req, res) => {
   res.send('Hello Word !!!')
 })
 
+//listenning
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`)
 })
