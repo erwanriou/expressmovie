e63682b183b29fc7a8fbc09657ea47bb3598433e
Erwan Riou <rr.1.riou@gmail.com>
2018-07-17 17:56:43 +0200

    build: hello word

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+app.get('/', (req, res) => {
+  res.send('Hello Word !!!')
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+})
