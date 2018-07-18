21d80d25069bf7eeb3b8fc10e2138fe183211c4d
Erwan Riou <rr.1.riou@gmail.com>
2018-07-18 09:51:48 +0200

    build: setting the render views on each route

--- server.js
+++ server.js
@@ -13,14 +13,18 @@ app.set('view engine', 'ejs')
 
 //routes
 app.get('/movies', (req, res) => {
-  res.send('Bientot des films ici meme')
+  res.render('movies')
+})
+app.get('/movies-details', (req, res) => {
+  res.render('movies-details')
 })
 app.get('/movies/add', (req, res) => {
   res.send('Ajouter des films ici même')
 })
 app.get('/movies/:id', (req, res) => {
   const id = req.params.id
-  res.send(`film numéro ${id}`)
+  //res.send(`film numéro ${id}`)
+  res.render('movies-details')
 })
 app.get('/', (req, res) => {
   //res.send('Hello <b>Word<b/> !!!')
