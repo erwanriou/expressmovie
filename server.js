cb7c13c07034ef659f006fbb0b18938c72b4cabf
Erwan Riou <rr.1.riou@gmail.com>
2018-07-18 11:25:36 +0200

    build: iterate value from array and integrating on a map on view engine

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.set('view engine', 'ejs')
 
 //routes
 app.get('/movies', (req, res) => {
-  res.render('movies')
+  const title = 'Films français des trente dernières annés'
+  const frenchMovies = [
+    { title: 'Le fabuleux destin d\'Amélie Poulain', year: 2001},
+    { title: 'Buffet froid', year: 1979},
+    { title: 'Le diner de cons', year: 1998},
+    { title: 'De rouille et d\'os', year: 2012},
+  ]
+  res.render('movies', { movies: frenchMovies, title: title })
 })
 app.get('/movies/add', (req, res) => {
   res.send('Ajouter des films ici même')
